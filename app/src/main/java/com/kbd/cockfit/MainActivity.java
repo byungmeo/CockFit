e2f87ab0bf634bb399f61b40be0f62bbb3bfb378
byungmeo <wlsrlfvkr@gmail.com>
2021-10-10 13:20:49 +0900

    개념설계보고서최종 & 기본Activity, Fragment 배치2

--- app/src/main/java/com/kbd/cockfit/MainActivity.java
+++ app/src/main/java/com/kbd/cockfit/MainActivity.java
@@ -2,11 +2,16 @@ package com.kbd.cockfit;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -16,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     private FragmentManager fragmentManager = getSupportFragmentManager();
     private MyRecipeFragment myRecipeFragment = new MyRecipeFragment();
     private CommunityFragment communityFragment = new CommunityFragment();
+    private CocktailInfoFragment cocktailInfoFragment = new CocktailInfoFragment();
+    private ProfileFragment profileFragment = new ProfileFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +34,22 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnItemSelectedListener(new ItemSelectedListener());
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getRealMetrics(displayMetrics);
+        int realDeviceHeight = displayMetrics.heightPixels;
+
+        int statusBarHeight = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) statusBarHeight = getResources().getDimensionPixelSize(resourceId);
+
+        int bottomBarHeight = 0;
+        int resourceIdBottom = getResources().getIdentifier("navigation_bar_height", "dimen", "android");
+        if (resourceIdBottom > 0) bottomBarHeight = getResources().getDimensionPixelSize(resourceIdBottom);
+
+        int frameHeight = realDeviceHeight - statusBarHeight - bottomNavigationView.getHeight() - bottomBarHeight;
+        FrameLayout fl = findViewById(R.id.frameLayout);
+        fl.setLayoutParams(new LinearLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, frameHeight));
     }
 
     class ItemSelectedListener implements NavigationBarView.OnItemSelectedListener {
@@ -37,11 +60,15 @@ public class MainActivity extends AppCompatActivity {
             switch (menuItem.getItemId()) {
                 case R.id.page_myRecipe:
                     transaction.replace(R.id.frameLayout, myRecipeFragment).commitAllowingStateLoss();
-
                     break;
                 case R.id.page_community:
                     transaction.replace(R.id.frameLayout, communityFragment).commitAllowingStateLoss();
                     break;
+                case R.id.page_cocktailInfo:
+                    transaction.replace(R.id.frameLayout, cocktailInfoFragment).commitAllowingStateLoss();
+                    break;
+                case R.id.page_profile:
+                    transaction.replace(R.id.frameLayout, profileFragment).commitAllowingStateLoss();
             }
             return true;
         }
