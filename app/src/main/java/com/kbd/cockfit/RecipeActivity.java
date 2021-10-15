d01d27ec49ed6bfff3ebea1610a6c93ebfe90c92
DESKTOP-ABBALQ4\choij <choijh0703@naver.com>
2021-10-15 13:13:34 +0900

    레시피 액티비티 UI구상

--- app/src/main/java/com/kbd/cockfit/RecipeActivity.java
+++ app/src/main/java/com/kbd/cockfit/RecipeActivity.java
@@ -1,8 +1,13 @@
 package com.kbd.cockfit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class RecipeActivity extends AppCompatActivity {
 
@@ -11,4 +16,10 @@ public class RecipeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
     }
+
+
+    public void onBackButtonTapped(View view) {
+        super.onBackPressed();
+    }
+
 }
