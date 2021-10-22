d6daaeb197dedaea818dca53733d2c41ee888f94
byungmeo <wlsrlfvkr@gmail.com>
2021-10-23 03:35:17 +0900

    프로젝트 패키지 폴더 간략화 (어댑터 등을 Activity에 통합)

--- app/src/main/java/com/kbd/cockfit/MyPostActivity.java
+++ app/src/main/java/com/kbd/cockfit/MyPostActivity.java
@@ -1,11 +1,16 @@
 package com.kbd.cockfit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -53,5 +58,72 @@ public class MyPostActivity extends AppCompatActivity {
 
     public void clickBackButton(View view) {
         onBackPressed();
+    }
+
+    public class MyPost {
+        private String postName;
+        private String postAbstract;
+        private String postDate;
+
+        public MyPost(String postName, String postAbstract, String postDate) {
+            this.postName = postName;
+            this.postAbstract = postAbstract;
+            this.postDate = postDate;
+        }
+
+        public String getPostName() {
+            return postName;
+        }
+
+        public String getPostAbstract() {
+            return postAbstract;
+        }
+
+        public String getPostDate() {
+            return postDate;
+        }
+    }
+
+    public class MyPostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+        private Context context;
+        private ArrayList<MyPost> myPostArrayList;
+
+        public MyPostAdapter(Context context, ArrayList<MyPost> myPostArrayList) {
+            this.context = context;
+            this.myPostArrayList = myPostArrayList;
+        }
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.mypostitem_layout, parent, false);
+            return new MyPostAdapter.MyPostViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            MyPostAdapter.MyPostViewHolder myPostViewHolder = (MyPostAdapter.MyPostViewHolder) holder;
+            myPostViewHolder.postName.setText(myPostArrayList.get(position).getPostName());
+            myPostViewHolder.postAbstract.setText(myPostArrayList.get(position).getPostAbstract());
+            myPostViewHolder.postDate.setText(myPostArrayList.get(position).getPostDate());
+        }
+
+        @Override
+        public int getItemCount() {
+            return myPostArrayList.size();
+        }
+
+        public class MyPostViewHolder extends RecyclerView.ViewHolder {
+            private TextView postName;
+            private TextView postAbstract;
+            private TextView postDate;
+
+            public MyPostViewHolder(@NonNull View itemView) {
+                super(itemView);
+                postName = itemView.findViewById(R.id.myPostItem_textView_postName);
+                postAbstract = itemView.findViewById(R.id.myPostItem_textView_postAbstract);
+                postDate = itemView.findViewById(R.id.myPostItem_textView_postDate);
+            }
+        }
     }
 }
