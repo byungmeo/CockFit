88149da4e813b9b847a0c54acba09fd711d423ca
DESKTOP-ABBALQ4\choij <choijh0703@naver.com>
2021-10-22 21:09:57 +0900

    나만의 레시피 제작 액티비티 UI보완, id값 수정

--- app/src/main/java/com/kbd/cockfit/MakeRecipeActivity.java
+++ app/src/main/java/com/kbd/cockfit/MakeRecipeActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentProvider;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,5 +18,19 @@ public class MakeRecipeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_recipe);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    public void clickBackButton(View view) {
+        onBackPressed();
+    }
+
+    public void clickTest(View view) {
+        Intent in = new Intent(this, RecipeActivity.class);
+        startActivity(in);
     }
 }
