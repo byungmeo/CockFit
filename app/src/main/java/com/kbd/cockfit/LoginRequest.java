15ac56b95fc8ad06183f63e8ba908936e996dafb
Minuoo <90535514+Minuoo@users.noreply.github.com>
2021-11-02 17:29:01 +0900

    db 테스트

--- app/src/main/java/com/kbd/cockfit/LoginRequest.java
+++ app/src/main/java/com/kbd/cockfit/LoginRequest.java
@@ -1,5 +1,13 @@
 package com.kbd.cockfit;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,7 +15,7 @@ import java.util.Map;
 public class LoginRequest extends StringRequest {
 
     //서버 URL 설정(php 파일 연동)
-    final static private String URL = "http:///cockfit.dothome.co.kr//cockfit_login.php";
+    final static private String URL = "http://cockfit.dothome.co.kr/cockfit_login.php";
     private Map<String, String> map;
 
     public LoginRequest(String user_id, String user_password, Response.Listener<String> listener) {
