b598229f8ee5fd92ebac5e6dcab15467a496737d
byungmeo <wlsrlfvkr@gmail.com>
2021-11-04 01:11:38 +0900

    LoginActivity 코드 간결화 및 개발자용 화면 전환 버튼 추가

--- app/src/main/java/com/kbd/cockfit/LoginActivity.java
+++ app/src/main/java/com/kbd/cockfit/LoginActivity.java
@@ -19,80 +19,85 @@ import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText login_editText_id, login_editText_pw;
-    private Button login_button_login, login_button_register;
+    private EditText editText_id; //id 입력창
+    private EditText editText_password; //pw 입력창
+    private Button button_login; //로그인 버튼
+    private Button button_register; //회원가입 버튼
+    private Button button_develop; //개발자용 임시 버튼
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.Theme_CockFit);
+        setTheme(R.style.Theme_CockFit); //Splash 화면 출력 후 본래 테마로 전환
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        EditText login_editText_id = (EditText) findViewById(R.id.login_editText_id);
-        EditText login_editText_pw = (EditText) findViewById(R.id.login_editText_pw);
+        editText_id = findViewById(R.id.login_editText_id);
+        editText_password = findViewById(R.id.login_editText_pw);
+        button_login = findViewById(R.id.login_button_login);
+        button_register =findViewById(R.id.login_button_register);
+        button_develop = findViewById(R.id.login_button_developButton);
 
-        login_button_login = (Button)findViewById(R.id.login_button_login);
-        login_button_login.setOnClickListener(new View.OnClickListener(){
+        //로그인 버튼에 리스너를 부착합니다.
+        button_login.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                String userID = login_editText_id.getText().toString();
-                String userPass = login_editText_pw.getText().toString();
+                String userID = editText_id.getText().toString();
+                String userPass = editText_password.getText().toString();
 
                 Response.Listener<String> responseListener = new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         try {
-                            JSONObject jsonObject = new JSONObject( response );
-                            boolean success = jsonObject.getBoolean( "success" );
-                            //Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_SHORT).show();
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean isSuccess = jsonObject.getBoolean("success");
 
-                            if(success) {  //로그인 성공시
-
-                                String user_id = jsonObject.getString( "user_id" );
-                                String user_password = jsonObject.getString( "user_password" );
-                                String user_nickname = jsonObject.getString( "user_nickname" );
+                            if(isSuccess) {  //로그인 성공시
+                                String user_id = jsonObject.getString("user_id");
+                                String user_password = jsonObject.getString("user_password");
+                                String user_nickname = jsonObject.getString("user_nickname");
 
 
-                                Toast.makeText( getApplicationContext(), "로그인 성공", Toast.LENGTH_SHORT ).show();
-                                Intent intent = new Intent( LoginActivity.this, MainActivity.class );
+                                Toast.makeText(getApplicationContext(), "로그인 성공", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
 
-                                intent.putExtra( "user_id", user_id );
-                                intent.putExtra( "user_password", user_password );
-                                intent.putExtra( "user_nickname", user_nickname );
+                                intent.putExtra("user_id", user_id);
+                                intent.putExtra("user_password", user_password);
+                                intent.putExtra("user_nickname", user_nickname);
 
                                 startActivity( intent );
-
                             } else {  //로그인 실패시
                                 Log.d("test", "로그인 실패");
                                 Toast.makeText( getApplicationContext(), "로그인 실패", Toast.LENGTH_SHORT).show();
                                 return;
                             }
-
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }
                 };
 
-                LoginRequest loginRequest = new LoginRequest( userID, userPass, responseListener );
-                RequestQueue queue = Volley.newRequestQueue( LoginActivity.this );
-                queue.add( loginRequest );
-
+                LoginRequest loginRequest = new LoginRequest(userID, userPass, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
             }
-
         });
 
-        login_button_register =(Button)findViewById(R.id.login_button_register);
-        login_button_register.setOnClickListener(new View.OnClickListener() {
+        //회원가입 버튼에 리스너를 부착합니다.
+        button_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Intent intent = new Intent( LoginActivity.this, RegisterActivity.class );
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity( intent );
-
             }
-
         });
 
+        //개발자용 임시 버튼에 리스너를 부착합니다.
+        //로그인 로직을 건너뜁니다.
+        button_develop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity( intent );
+            }
+        });
     }
-
 }
