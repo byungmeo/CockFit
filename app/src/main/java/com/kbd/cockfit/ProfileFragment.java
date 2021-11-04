530db161ce5755b2d44aa60bbacd00f4544baa67
byungmeo <wlsrlfvkr@gmail.com>
2021-11-04 17:47:50 +0900

    프로필 닉네임 표시

--- app/src/main/java/com/kbd/cockfit/ProfileFragment.java
+++ app/src/main/java/com/kbd/cockfit/ProfileFragment.java
@@ -4,28 +4,68 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.os.Debug;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class ProfileFragment extends Fragment implements View.OnClickListener {
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+
+    private TextView editText_nickname;
+
+    private Button button_myFavorite;
+    private Button button_myPost;
+    private Button button_myComment;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        Button favorite = v.findViewById(R.id.profile_button_recipe);
-        Button post = v.findViewById(R.id.profile_button_post);
-        Button comment = v.findViewById(R.id.profile_button_comment);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
 
-        favorite.setOnClickListener(this);
-        post.setOnClickListener(this);
-        comment.setOnClickListener(this);
+        editText_nickname = v.findViewById(R.id.profile_textView_name);
+        mDatabase.child("user").child(mAuth.getUid()).child("name").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                    editText_nickname.setText(String.valueOf(task.getResult().getValue()));
+                }
+            }
+        });
+
+        button_myFavorite = v.findViewById(R.id.profile_button_recipe);
+        button_myPost = v.findViewById(R.id.profile_button_post);
+        button_myComment = v.findViewById(R.id.profile_button_comment);
+
+        button_myFavorite.setOnClickListener(this);
+        button_myPost.setOnClickListener(this);
+        button_myComment.setOnClickListener(this);
 
         return v;
     }
--- app/src/main/java/com/kbd/cockfit/RegisterActivity.java
+++ app/src/main/java/com/kbd/cockfit/RegisterActivity.java
@@ -19,11 +19,16 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private DatabaseReference mDatabase;
     private FirebaseAuth mAuth;
     private ConstraintLayout con;
     private ImageView back;
@@ -36,6 +41,7 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         mAuth = FirebaseAuth.getInstance();
         con = findViewById(R.id.register_layout_const);
         back = findViewById(R.id.register_button_backButton);
@@ -95,6 +101,7 @@ public class RegisterActivity extends AppCompatActivity {
                         FirebaseUser user = mAuth.getCurrentUser();
                         Toast.makeText(RegisterActivity.this, "당신의 아이디는" + user.getEmail() + "입니다", Toast.LENGTH_SHORT).show();
                         user.sendEmailVerification();
+                        DataSet(true, editText_nickname.getText().toString());
                         Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                         startActivity(intent);
                     } else {
@@ -109,5 +116,26 @@ public class RegisterActivity extends AppCompatActivity {
     public void HideKeyboard(View view){
         InputMethodManager imm = (InputMethodManager)getSystemService(this.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(editText_email.getWindowToken(), 0);
+    }
+
+    public void DataSet(Boolean firstLaunch, String nickname) {
+        FirebaseUser user = mAuth.getCurrentUser(); //현재 로그인한 사람의 정보를 가져옴
+        final String uid = user.getUid(); //uid는 계정 하나당 하나의 값을 가짐
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //데이터 변경이 감지가 되면 이 함수가 자동으로 콜백이 됩니다 이때 dataSnapashot 는 값을 내려 받을떄 사용함으로 지금은 쓰지 않습니
+                mDatabase.child("user").child(uid).child("name").setValue(nickname);
+                mDatabase.child("user").child(uid).child("firstLunch").setValue(firstLaunch);
+                //RealTimeDB는 기본적으로 parent , child , value 값으로 이루어져 있습니다 지금은 최초로 로그인한 사람의
+                //색인을 만들고자 지금과 같은 작업을 하는 중입니다 즉 처음 들어오는 사람에게 DB자리를 내준다고 생각하시면됩니다
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // RealTimeDB와 통신 에러 등등 데이터를 정상적으로 받지 못할때 콜백함수로서 이곳으로 들어옵니다
+            }
+        });
     }
 }
