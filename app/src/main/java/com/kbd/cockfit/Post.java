8fbf9cb5cb376654b8cc7286ce30c1f7f12495f6
byungmeo <wlsrlfvkr@gmail.com>
2021-11-06 02:18:05 +0900

    게시글보기 구현

--- app/src/main/java/com/kbd/cockfit/Post.java
+++ app/src/main/java/com/kbd/cockfit/Post.java
@@ -1,46 +1,84 @@
 package com.kbd.cockfit;
 
-public class Post {
-    private int postNumber;
-    private String postTitle;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Post implements Parcelable {
+    private int number;
+    private String title;
     private String writer;
-    private String postDate;
-    private String postContent;
+    private String date;
+    private String content;
 
     public Post() {
 
     }
 
-    public Post(String postTitle, String writer, String postDate) {
-        this.postTitle = postTitle;
+    public Post(String title, String writer, String date) {
+        this.number = 0;
+        this.title = title;
         this.writer = writer;
-        this.postDate = postDate;
+        this.date = date;
+        this.content = "";
     }
 
-    public Post(int postNumber, String postTitle, String postDate, String postContent, String writer) {
-        this.postNumber = postNumber;
-        this.postTitle = postTitle;
+    public Post(int number, String title, String date, String content, String writer) {
+        this.number = number;
+        this.title = title;
         this.writer = writer;
-        this.postDate = postDate;
-        this.postContent = postContent;
+        this.date = date;
+        this.content = content;
     }
 
-    public int getPostNumber() { return postNumber; }
-    public String getPostTitle() {
-        return postTitle;
+    protected Post(Parcel in) {
+        number = in.readInt();
+        title = in.readString();
+        writer = in.readString();
+        date = in.readString();
+        content = in.readString();
+    }
+
+    public static final Creator<Post> CREATOR = new Creator<Post>() {
+        @Override
+        public Post createFromParcel(Parcel in) {
+            return new Post(in);
+        }
+
+        @Override
+        public Post[] newArray(int size) {
+            return new Post[size];
+        }
+    };
+
+    public int getNumber() { return number; }
+    public String getTitle() {
+        return title;
     }
     public String getWriter() { return writer; }
-    public String getPostDate() {
-        return postDate;
+    public String getDate() {
+        return date;
     }
-    public String getPostContent() { return postContent; }
+    public String getContent() { return content; }
 
-    public void setPostNumber(int number) { this.postNumber = number; }
-    public void setPostTitle(String postTitle) { this.postTitle = postTitle; }
-    public void setWriter(String nickname) { this.writer = nickname; }
-    public void setPostDate(String postDate) { this.postDate = postDate; }
-    public void setPostContent(String postContent) {
-        this.postContent = postContent;
+    public void setNumber(int number) { this.number = number; }
+    public void setTitle(String title) { this.title = title; }
+    public void setWriter(String writer) { this.writer = writer; }
+    public void setDate(String date) { this.date = date; }
+    public void setContent(String content) {
+        this.content = content;
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(number);
+        dest.writeString(title);
+        dest.writeString(writer);
+        dest.writeString(date);
+        dest.writeString(content);
+    }
 }
