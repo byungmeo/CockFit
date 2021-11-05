e51f874c11eefa4b55197765767cabf06ddf8557
Minuoo <90535514+Minuoo@users.noreply.github.com>
2021-11-05 16:43:28 +0900

    닉네임 변경 기능 구현

--- app/src/main/java/com/kbd/cockfit/ProfileFragment.java
+++ app/src/main/java/com/kbd/cockfit/ProfileFragment.java
@@ -1,25 +1,29 @@
 package com.kbd.cockfit;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import android.os.Debug;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -32,6 +36,8 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
     private FirebaseAuth mAuth;
 
     private TextView editText_nickname;
+
+    private ImageButton imageButton_validate;
 
     private Button button_myFavorite;
     private Button button_myPost;
@@ -50,10 +56,13 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         editText_nickname = v.findViewById(R.id.profile_textView_name);
         editText_nickname.setText(user.getDisplayName());
 
+        imageButton_validate = v.findViewById(R.id.profile_imageButton_validate);
+
         button_myFavorite = v.findViewById(R.id.profile_button_recipe);
         button_myPost = v.findViewById(R.id.profile_button_post);
         button_myComment = v.findViewById(R.id.profile_button_comment);
 
+        imageButton_validate.setOnClickListener(this);
         button_myFavorite.setOnClickListener(this);
         button_myPost.setOnClickListener(this);
         button_myComment.setOnClickListener(this);
@@ -65,6 +74,58 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
     public void onClick(View v) {
         Context context = v.getContext();
         switch (v.getId()) {
+            case R.id.profile_imageButton_validate: {
+                final EditText editText_changeNic = new EditText(context);
+
+                FrameLayout container = new FrameLayout(context);
+                FrameLayout.LayoutParams params = new  FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                params.leftMargin = getResources().getDimensionPixelSize(R.dimen.dialog_margin);
+                params.rightMargin = getResources().getDimensionPixelSize(R.dimen.dialog_margin);
+                editText_changeNic.setLayoutParams(params);
+                container.addView(editText_changeNic);
+
+                AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+
+                alertBuilder.setTitle("닉네임을 변경해주세요");
+                alertBuilder.setView(container);
+
+                alertBuilder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        final FirebaseUser user = mAuth.getCurrentUser();
+                        String userChangeNic = editText_changeNic.getText().toString();
+
+                        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(userChangeNic)
+                                .build();
+
+                        user.updateProfile(profileUpdates)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if(task.isSuccessful()) {
+                                            Toast.makeText(context, "닉네임이 변경되었습니다", Toast.LENGTH_SHORT).show();
+                                            editText_nickname.setText(userChangeNic);
+                                        }
+                                    }
+                                });
+                    }
+                });
+
+                alertBuilder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        Toast.makeText(context, "닉네임이 변경되지 않았습니다", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                AlertDialog alert = alertBuilder.create();
+                alert.show();
+
+                break;
+            }
+
+
             case R.id.profile_button_recipe: {
                 Intent intent = new Intent(context, ListActivity.class);
                 intent.putExtra("keyword", "favorite");
