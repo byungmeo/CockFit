5bd8114bbd32148f790f4943436bb94d4bd38b89
IMJayC <choijh0703@naver.com>
2021-11-19 16:19:19 +0900

    칵테일 기본상식 액티비티 구현, 칵테일 기법 액티비티 구현, json파일 수정

---
+++ app/src/main/java/com/kbd/cockfit/TechniqueActivity.java
@@ -0,0 +1,13 @@
+package com.kbd.cockfit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TechniqueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_technique);
+    }
+}
