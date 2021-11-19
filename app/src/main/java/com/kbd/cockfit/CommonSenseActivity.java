16abcfc38f7a3c35793df5adb25179481a04bd70
IMJayC <choijh0703@naver.com>
2021-11-19 23:13:41 +0900

    기본상식, 기주, 기법 액티비티 앱바 구현

--- app/src/main/java/com/kbd/cockfit/CommonSenseActivity.java
+++ app/src/main/java/com/kbd/cockfit/CommonSenseActivity.java
@@ -1,20 +1,42 @@
 package com.kbd.cockfit;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class CommonSenseActivity extends AppCompatActivity {
+
+    private Toolbar commonsensetoolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_common_sense);
+
+        commonsensetoolbar = findViewById(R.id.commonsense_materialToolbar);
+        setSupportActionBar(commonsensetoolbar);
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        commonsensetoolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CommonSenseActivity.this.onBackPressed();
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
     public void clickButton(View view) {
         if(view.getId() == R.id.commonsense_button_backButton) {
             this.onBackPressed();
