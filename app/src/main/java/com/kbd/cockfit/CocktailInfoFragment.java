4ee4048794584e1aa11416f559a70ddc44b44dfa
weejinyoung <a8118199@gmail.com>
2021-11-19 19:42:27 +0900

    프래그먼트 앱바 구현, 대표컬러 적용, 칵테일인포 프래그먼트 디자인 개선

--- app/src/main/java/com/kbd/cockfit/CocktailInfoFragment.java
+++ app/src/main/java/com/kbd/cockfit/CocktailInfoFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.fragment.app.Fragment;
 
@@ -18,9 +19,9 @@ public class CocktailInfoFragment extends Fragment implements View.OnClickListen
         View v = inflater.inflate(R.layout.fragment_cocktail_info, container, false);
 
 
-        AppCompatImageButton listButton = v.findViewById(R.id.cock_listButton);
-        AppCompatImageButton ingrdButton = v.findViewById(R.id.cock_ingrdButton);
-        AppCompatImageButton equipButton = v.findViewById(R.id.cock_equipButton);
+        AppCompatButton listButton = v.findViewById(R.id.cock_listButton);
+        AppCompatButton ingrdButton = v.findViewById(R.id.cock_ingrdButton);
+        AppCompatButton equipButton = v.findViewById(R.id.cock_equipButton);
         listButton.setOnClickListener(this);
         ingrdButton.setOnClickListener(this);
         equipButton.setOnClickListener(this);
